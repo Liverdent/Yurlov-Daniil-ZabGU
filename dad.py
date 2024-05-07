125acd1d0d32fc5fc9865f6d8634fb909d7d868b
Liverdent <97013045+Liverdent@users.noreply.github.com>
2024-05-07 09:27:08 +0900

    Add files via upload

--- dad.py
+++ dad.py
@@ -1,11 +1,23 @@
 import pygame
 
+class player():
+    def __init__(self, position=(0, 0), scale=(10, 10)):
+        self.position = position
+        self.scale = scale
+
+        body = pygame.Rect(self.position, self.scale)
+        sprite = capsule
+        player = {body: body, sprite: sprite}
+
+
+
+
 
 def look_at(object, coords):
     pass
 
 
-def capsule(surface, x = 0, y=0, radius=30, heigh=70):
+def capsule(surface, x=0, y=0, radius=30, heigh=70):
     heigh = max(heigh, 2 * radius)
     pygame.draw.circle(surface, 'darkgrey', (x + radius, y + heigh - radius), radius)
     pygame.draw.rect(surface, 'white', (x, y + radius, 2 * radius, heigh - 2 * radius))
@@ -21,11 +33,14 @@ screen = pygame.display.set_mode((screen_size_x, screen_size_y))
 #player = pygame.surface
 done = False
 
-capsule(screen, 60, 60, 40, 90)
+a = pygame.Rect(10, 10, 20, 20)
+capsule(screen, 60, 60, 40, 200)
 
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+        if event.type == pygame.MOUSEMOTION:
+            pass
 
     pygame.display.flip()
