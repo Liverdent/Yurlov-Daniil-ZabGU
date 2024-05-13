893f3041c98596378d688e49dc5425e36ebbd28c
Liverdent <97013045+Liverdent@users.noreply.github.com>
2024-05-13 22:25:32 +0900

    добавлено графическое отображение возможного хода
    Исправлены ошибки
    
    Теперь можно ходить только на определенные поля
    
    Добавлено отображение возможных ходов

--- dad.py
+++ dad.py
@@ -49,7 +49,6 @@ class desk:
                       self.line22, self.line2, self.line31, self.line32, self.screen_size - self.indent]
         self.big_lines = [self.indent, self.line1, self.line2, self.screen_size - self.indent]
         # fields
-
         self.cur_field = None
         self.fields = [[None] * 9 for _ in range(9)]
         self.field = [None] * 9
@@ -73,6 +72,7 @@ class desk:
     def change_player(self):
         self.cur_player = 'cross' if self.cur_player == 'zero' else 'zero'
 
+    # noinspection PyTypeChecker
     def set_fig(self, position):
         pos = [min(max(i, self.indent), self.screen_size - self.indent)for i in position]
         pos = [(int(i // ((self.screen_size - 2 * self.indent) / 9))) for i in pos]
@@ -80,15 +80,30 @@ class desk:
 
         pos_ = int(9 * (pos[1] - 1) + pos[0] - 1)
         new_pos = self.translate[pos_]
-
-        if self.fields[new_pos[0]][new_pos[1]] is not None:
+        if ((self.fields[new_pos[0]][new_pos[1]] or self.field[new_pos[0]]) is not None
+                and (new_pos[0] != self.cur_field and new_pos[0] is not None)):
             return
+        if new_pos[0] == self.cur_field or self.cur_field is None:
+            self.draw_fig(True, pos)
+            self.fields[new_pos[0]][new_pos[1]] = self.cur_player
+            self.cur_field = new_pos
+            self.check_desk()
+            self.change_player()
+            self.change_cur_field(new_pos)
 
-        self.draw_fig(True, pos)
-        self.fields[new_pos[0]][new_pos[1]] = self.cur_player
-        self.cur_field = new_pos
-        self.check_desk()
-        self.change_player()
+    def change_cur_field(self, position):
+        for i in range(9):
+            if self.field[i] is None:
+                self.draw_small_field((100, 100, 100), i)
+        if ((self.field[position[0]] is not None) or (None not in self.fields[position[0]])
+                or (self.field[position[1]] is not None)):
+            self.cur_field = None
+            for i in range(9):
+                if self.field[i] is None:
+                    self.draw_small_field('yellow', i)
+        else:
+            self.cur_field = position[1]
+            self.draw_small_field('yellow', self.cur_field)
 
     def set_fig_big(self, field):
         pos = self.translate_big[field]
@@ -109,11 +124,16 @@ class desk:
             xy4 = [xy1[0], xy2[1]]
         else:
             width *= 3
+
             xy1 = [self.big_lines[pos] - self.indent / 3 for pos in position]
             position = [pos - 1 for pos in position]
             xy2 = [self.big_lines[pos] + self.indent / 3 for pos in position]
             xy3 = [xy2[0], xy1[1]]
             xy4 = [xy1[0], xy2[1]]
+
+            pos = [i - self.indent / 9 for i in xy2]
+            size = [abs(xy2[0] - xy1[0]) + self.indent / 4] * 2
+            self.surface.fill((0, 0, 0, 50), pos + size)
 
         centre = [(xy1[i] + xy2[i]) / 2 for i in range(2)]
         radius = abs(xy2[0] - xy1[0]) / 2
@@ -141,7 +161,7 @@ class desk:
             if self.check_grid(self.fields[field]):
                 self.set_fig_big(field)
 
-    def draw_(self, color='purple'):
+    def draw_big_field(self, color='grey'):
         pygame.draw.line(self.surface, color, (self.indent, self.line1),
                          (self.screen_size - self.indent, self.line1), 5)
         pygame.draw.line(self.surface, color, (self.indent, self.line2),
@@ -152,113 +172,117 @@ class desk:
         pygame.draw.line(self.surface, color, (self.line1, self.indent),
                          (self.line1, self.screen_size - self.indent), 5)
 
-        pygame.draw.line(self.surface, color, (self.indent * 1.3, self.line11),
-                         (self.line1 - self.indent * 0.3, self.line11), 3)
-        pygame.draw.line(self.surface, color, (self.line1 + self.indent * 0.3, self.line11),
-                         (self.line2 - self.indent * 0.3, self.line11), 3)
-        pygame.draw.line(self.surface, color, (self.line2 + self.indent * 0.3, self.line11),
-                         (self.screen_size - self.indent * 1.3, self.line11), 3)
+    def draw_small_field(self, color, index):
+        if index == 0:
+            pygame.draw.line(self.surface, color, (self.indent * 1.3, self.line11),
+                             (self.line1 - self.indent * 0.3, self.line11), 3)
+            pygame.draw.line(self.surface, color, (self.indent * 1.3, self.line12),
+                             (self.line1 - self.indent * 0.3, self.line12), 3)
+            pygame.draw.line(self.surface, color, (self.line12, self.indent * 1.3),
+                             (self.line12, self.line1 - self.indent * 0.3), 3)
+            pygame.draw.line(self.surface, color, (self.line11, self.indent * 1.3),
+                             (self.line11, self.line1 - self.indent * 0.3), 3)
+        elif index == 1:
+            pygame.draw.line(self.surface, color, (self.line1 + self.indent * 0.3, self.line11),
+                             (self.line2 - self.indent * 0.3, self.line11), 3)
+            pygame.draw.line(self.surface, color, (self.line1 + self.indent * 0.3, self.line12),
+                             (self.line2 - self.indent * 0.3, self.line12), 3)
+            pygame.draw.line(self.surface, color, (self.line21, self.indent * 1.3),
+                             (self.line21, self.line1 - self.indent * 0.3), 3)
+            pygame.draw.line(self.surface, color, (self.line22, self.indent * 1.3),
+                             (self.line22, self.line1 - self.indent * 0.3), 3)
+        elif index == 2:
+            pygame.draw.line(self.surface, color, (self.line2 + self.indent * 0.3, self.line11),
+                             (self.screen_size - self.indent * 1.3, self.line11), 3)
+            pygame.draw.line(self.surface, color, (self.line2 + self.indent * 0.3, self.line12),
+                             (self.screen_size - self.indent * 1.3, self.line12), 3)
+            pygame.draw.line(self.surface, color, (self.line31, self.indent * 1.3),
+                             (self.line31, self.line1 - self.indent * 0.3), 3)
+            pygame.draw.line(self.surface, color, (self.line32, self.indent * 1.3),
+                             (self.line32, self.line1 - self.indent * 0.3), 3)
+        elif index == 3:
+            pygame.draw.line(self.surface, color, (self.indent * 1.3, self.line21),
+                             (self.line1 - self.indent * 0.3, self.line21), 3)
+            pygame.draw.line(self.surface, color, (self.indent * 1.3, self.line22),
+                             (self.line1 - self.indent * 0.3, self.line22), 3)
+            pygame.draw.line(self.surface, color, (self.line11, self.line1 + self.indent * 0.3),
+                             (self.line11, self.line2 - self.indent * 0.3), 3)
+            pygame.draw.line(self.surface, color, (self.line12, self.line1 + self.indent * 0.3),
+                             (self.line12, self.line2 - self.indent * 0.3), 3)
+        elif index == 4:
+            pygame.draw.line(self.surface, color, (self.line1 + self.indent * 0.3, self.line21),
+                             (self.line2 - self.indent * 0.3, self.line21), 3)
+            pygame.draw.line(self.surface, color, (self.line21, self.line1 + self.indent * 0.3),
+                             (self.line21, self.line2 - self.indent * 0.3), 3)
+            pygame.draw.line(self.surface, color, (self.line1 + self.indent * 0.3, self.line22),
+                             (self.line2 - self.indent * 0.3, self.line22), 3)
+            pygame.draw.line(self.surface, color, (self.line22, self.line1 + self.indent * 0.3),
+                             (self.line22, self.line2 - self.indent * 0.3), 3)
+        elif index == 5:
+            pygame.draw.line(self.surface, color, (self.line31, self.line1 + self.indent * 0.3),
+                             (self.line31, self.line2 - self.indent * 0.3), 3)
+            pygame.draw.line(self.surface, color, (self.line32, self.line1 + self.indent * 0.3),
+                             (self.line32, self.line2 - self.indent * 0.3), 3)
+            pygame.draw.line(self.surface, color, (self.line2 + self.indent * 0.3, self.line22),
+                             (self.screen_size - self.indent * 1.3, self.line22), 3)
+            pygame.draw.line(self.surface, color, (self.line2 + self.indent * 0.3, self.line21),
+                             (self.screen_size - self.indent * 1.3, self.line21), 3)
+        elif index == 6:
+            pygame.draw.line(self.surface, color, (self.line11, self.line2 + self.indent * 0.3),
+                             (self.line11, self.screen_size - self.indent * 1.3), 3)
+            pygame.draw.line(self.surface, color, (self.line12, self.line2 + self.indent * 0.3),
+                             (self.line12, self.screen_size - self.indent * 1.3), 3)
+            pygame.draw.line(self.surface, color, (self.indent * 1.3, self.line31),
+                             (self.line1 - self.indent * 0.3, self.line31), 3)
+            pygame.draw.line(self.surface, color, (self.indent * 1.3, self.line32),
+                             (self.line1 - self.indent * 0.3, self.line32), 3)
+        elif index == 7:
+            pygame.draw.line(self.surface, color, (self.line21, self.line2 + self.indent * 0.3),
+                             (self.line21, self.screen_size - self.indent * 1.3), 3)
+            pygame.draw.line(self.surface, color, (self.line22, self.line2 + self.indent * 0.3),
+                             (self.line22, self.screen_size - self.indent * 1.3), 3)
+            pygame.draw.line(self.surface, color, (self.line1 + self.indent * 0.3, self.line31),
+                             (self.line2 - self.indent * 0.3, self.line31), 3)
+            pygame.draw.line(self.surface, color, (self.line1 + self.indent * 0.3, self.line32),
+                             (self.line2 - self.indent * 0.3, self.line32), 3)
+        elif index == 8:
+            pygame.draw.line(self.surface, color, (self.line2 + self.indent * 0.3, self.line31),
+                             (self.screen_size - self.indent * 1.3, self.line31), 3)
+            pygame.draw.line(self.surface, color, (self.line2 + self.indent * 0.3, self.line32),
+                             (self.screen_size - self.indent * 1.3, self.line32), 3)
+            pygame.draw.line(self.surface, color, (self.line31, self.line2 + self.indent * 0.3),
+                             (self.line31, self.screen_size - self.indent * 1.3), 3)
+            pygame.draw.line(self.surface, color, (self.line32, self.line2 + self.indent * 0.3),
+                             (self.line32, self.screen_size - self.indent * 1.3), 3)
 
-        pygame.draw.line(self.surface, color, (self.indent * 1.3, self.line12),
-                         (self.line1 - self.indent * 0.3, self.line12), 3)
-        pygame.draw.line(self.surface, color, (self.line1 + self.indent * 0.3, self.line12),
-                         (self.line2 - self.indent * 0.3, self.line12), 3)
-        pygame.draw.line(self.surface, color, (self.line2 + self.indent * 0.3, self.line12),
-                         (self.screen_size - self.indent * 1.3, self.line12), 3)
-
-        pygame.draw.line(self.surface, color, (self.indent * 1.3, self.line21),
-                         (self.line1 - self.indent * 0.3, self.line21), 3)
-        pygame.draw.line(self.surface, color, (self.line1 + self.indent * 0.3, self.line21),
-                         (self.line2 - self.indent * 0.3, self.line21), 3)
-        pygame.draw.line(self.surface, color, (self.line2 + self.indent * 0.3, self.line21),
-                         (self.screen_size - self.indent * 1.3, self.line21), 3)
-
-        pygame.draw.line(self.surface, color, (self.indent * 1.3, self.line22),
-                         (self.line1 - self.indent * 0.3, self.line22), 3)
-        pygame.draw.line(self.surface, color, (self.line1 + self.indent * 0.3, self.line22),
-                         (self.line2 - self.indent * 0.3, self.line22), 3)
-        pygame.draw.line(self.surface, color, (self.line2 + self.indent * 0.3, self.line22),
-                         (self.screen_size - self.indent * 1.3, self.line22), 3)
-
-        pygame.draw.line(self.surface, color, (self.indent * 1.3, self.line31),
-                         (self.line1 - self.indent * 0.3, self.line31), 3)
-        pygame.draw.line(self.surface, color, (self.line1 + self.indent * 0.3, self.line31),
-                         (self.line2 - self.indent * 0.3, self.line31), 3)
-        pygame.draw.line(self.surface, color, (self.line2 + self.indent * 0.3, self.line31),
-                         (self.screen_size - self.indent * 1.3, self.line31), 3)
-
-        pygame.draw.line(self.surface, color, (self.indent * 1.3, self.line32),
-                         (self.line1 - self.indent * 0.3, self.line32), 3)
-        pygame.draw.line(self.surface, color, (self.line1 + self.indent * 0.3, self.line32),
-                         (self.line2 - self.indent * 0.3, self.line32), 3)
-        pygame.draw.line(self.surface, color, (self.line2 + self.indent * 0.3, self.line32),
-                         (self.screen_size - self.indent * 1.3, self.line32), 3)
-
-        pygame.draw.line(self.surface, color, (self.line11, self.indent * 1.3),
-                         (self.line11, self.line1 - self.indent * 0.3), 3)
-        pygame.draw.line(self.surface, color, (self.line11, self.line1 + self.indent * 0.3),
-                         (self.line11, self.line2 - self.indent * 0.3), 3)
-        pygame.draw.line(self.surface, color, (self.line11, self.line2 + self.indent * 0.3),
-                         (self.line11, self.screen_size - self.indent * 1.3), 3)
-
-        pygame.draw.line(self.surface, color, (self.line12, self.indent * 1.3),
-                         (self.line12, self.line1 - self.indent * 0.3), 3)
-        pygame.draw.line(self.surface, color, (self.line12, self.line1 + self.indent * 0.3),
-                         (self.line12, self.line2 - self.indent * 0.3), 3)
-        pygame.draw.line(self.surface, color, (self.line12, self.line2 + self.indent * 0.3),
-                         (self.line12, self.screen_size - self.indent * 1.3), 3)
-
-        pygame.draw.line(self.surface, color, (self.line21, self.indent * 1.3),
-                         (self.line21, self.line1 - self.indent * 0.3), 3)
-        pygame.draw.line(self.surface, color, (self.line21, self.line1 + self.indent * 0.3),
-                         (self.line21, self.line2 - self.indent * 0.3), 3)
-        pygame.draw.line(self.surface, color, (self.line21, self.line2 + self.indent * 0.3),
-                         (self.line21, self.screen_size - self.indent * 1.3), 3)
-
-        pygame.draw.line(self.surface, color, (self.line22, self.indent * 1.3),
-                         (self.line22, self.line1 - self.indent * 0.3), 3)
-        pygame.draw.line(self.surface, color, (self.line22, self.line1 + self.indent * 0.3),
-                         (self.line22, self.line2 - self.indent * 0.3), 3)
-        pygame.draw.line(self.surface, color, (self.line22, self.line2 + self.indent * 0.3),
-                         (self.line22, self.screen_size - self.indent * 1.3), 3)
-
-        pygame.draw.line(self.surface, color, (self.line31, self.indent * 1.3),
-                         (self.line31, self.line1 - self.indent * 0.3), 3)
-        pygame.draw.line(self.surface, color, (self.line31, self.line1 + self.indent * 0.3),
-                         (self.line31, self.line2 - self.indent * 0.3), 3)
-        pygame.draw.line(self.surface, color, (self.line31, self.line2 + self.indent * 0.3),
-                         (self.line31, self.screen_size - self.indent * 1.3), 3)
-
-        pygame.draw.line(self.surface, color, (self.line32, self.indent * 1.3),
-                         (self.line32, self.line1 - self.indent * 0.3), 3)
-        pygame.draw.line(self.surface, color, (self.line32, self.line1 + self.indent * 0.3),
-                         (self.line32, self.line2 - self.indent * 0.3), 3)
-        pygame.draw.line(self.surface, color, (self.line32, self.line2 + self.indent * 0.3),
-                         (self.line32, self.screen_size - self.indent * 1.3), 3)
+    def draw_(self, color='grey', sub_color=(100, 100, 100)):
+        self.draw_big_field(color)
+        for i in range(9):
+            self.draw_small_field(sub_color, i)
 
 
-pygame.init()
+if __name__ == '__main__':
+    pygame.init()
 
-screen_size = 600
-screen = pygame.display.set_mode((screen_size, screen_size))
+    screen_size = 600
+    screen = pygame.display.set_mode((screen_size, screen_size))
 
-layer = pygame.Surface((screen_size, screen_size), pygame.SRCALPHA)
+    layer = pygame.Surface((screen_size, screen_size), pygame.SRCALPHA)
 
-player = "Потом"
+    player = "Потом"
 
-Desk = desk(layer, screen_size, 50,  player)
+    Desk = desk(layer, screen_size, 50,  player)
 
-done = False
+    done = False
+    Desk.draw_('grey', (100, 100, 100))
+    while not done:
+        pygame.time.Clock().tick(30)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
 
-while not done:
-    pygame.time.Clock().tick(10)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                Desk.set_fig(pygame.mouse.get_pos())
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            Desk.set_fig(pygame.mouse.get_pos())
-
-    Desk.draw_('grey')
-    screen.blit(layer, (0, 0))
-    pygame.display.flip()
+        screen.blit(layer, (0, 0))
+        pygame.display.flip()
